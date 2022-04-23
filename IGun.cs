86956e3acd1b9e8e9bc7862df2bf120959b99170
MNYOU <Arman.Mukanov@urfu.me>
2022-04-24 00:03:39 +0500

    добавлен интерфейс

---
+++ IGun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СrazyKiller
+{
+    public interface IGun
+    {
+        int Damage { get; set; }
+        int Ammunition { get; set; }
+        double Recharge { get; set; }
+    }
+}
