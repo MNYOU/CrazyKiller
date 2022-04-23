bf01393d1e59e858047e51a2de0a0d3c0dcb30d1
MNYOU <Arman.Mukanov@urfu.me>
2022-04-24 00:04:54 +0500

    ...

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CrazyKiller
 {
-    static class Program
+    static class Program // добавлена отрисовка карты и персонажа, now player can move
     {
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var player = new Player(20, new Point(160, 60));
+            var game = new GameModel(player);
+            Application.Run(new GameForm(game));
         }
     }
 }
