8f5028d753cdb746b31705e091dbde5bf05fb988
MNYOU <Arman.Mukanov@urfu.me>
2022-04-24 00:03:02 +0500

    добавлен класс

--- Gun.cs
+++ Gun.cs
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace СrazyKiller
 {
-    class Gun
+    public class Gun
     {
 
+    }
+
+    public class Pistol : Gun, IGun
+    {
+        public int Damage { get; set; }
+        public int Ammunition { get; set; }
+        public double Recharge { get; set; }
+
+        public Pistol(int damage, int ammunition, double recharge)
+        {
+            Damage = damage;
+            Ammunition = ammunition;
+            Recharge = recharge;
+        }
+
+    }
+
+    public class MachineGun : IGun
+    {
+        public int Damage { get;  set; }
+        public int Ammunition { get; set; }
+        public double Recharge { get; set; }
+
+        public MachineGun(int damage, int ammunition, double recharge)
+        {
+            Damage = damage;
+            Ammunition = ammunition;
+            Recharge = recharge;
+        }
+    }
+
+    public class Shotgun : IGun
+    {
+        public int Damage { get; set; }
+        public int Ammunition { get; set; }
+        public double Recharge { get; set; }
+
+        public Shotgun(int damage, int ammunition, double recharge)
+        {
+            Damage = damage;
+            Ammunition = ammunition;
+            Recharge = recharge;
+        }
     }
 }
---
+++ Timer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сrazy_killer
+{
+    class Timer
+    {
+        public readonly int Tick;
+
+        public Timer(int tick)
+        {
+            Tick = tick;
+        }
+
+        public void Start()
+        {
+
+        }
+    }
+}
