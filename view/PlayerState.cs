ad202c993f959ed67007aba16ddad0b40356025c
MNYOU <Arman.Mukanov@urfu.me>
2022-05-27 00:49:39 +0500

    добавлен новый список

---
+++ view/PlayerState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyKiller
+{
+    public enum PlayerState
+    {
+        Attack,
+        Run,
+        AttackAndRun,
+        Reloading,
+        Stand,
+        Hurt
+    }
+}
