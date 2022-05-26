dd7746c8de248c6f735459c10c1b70c664d9deb2
MNYOU <Arman.Mukanov@urfu.me>
2022-05-27 00:44:20 +0500

    добавлены уровни

---
+++ model/Level.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyKiller
+{
+    public class Level
+    {
+        public readonly int Number;
+        public readonly int CountZombies;
+        private static int degreeIncrease = 5;
+
+        public Level(int number)
+        {
+            Number = number;
+            CountZombies = number * degreeIncrease;
+            //CountZombies = (int)Math.Pow(2, number);
+        }
+    }
+}
