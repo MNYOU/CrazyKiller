deb443992a962b07d29678ebbffac480a870a1d2
MNYOU <Arman.Mukanov@urfu.me>
2022-05-27 00:46:15 +0500

    добавлены уровни(волны)

--- model/GameModel.cs
+++ model/GameModel.cs
@@ -12,13 +12,19 @@ namespace CrazyKiller
     public class GameModel
     {
         public readonly Player Player;
+        public List<Golem> Zombies;
+        public Level[] levels;
+        public Level currentLevel;
+        public MedKit MedKit;
+        public Box Box;
         public Size Size;
         public static Size WindowSize;
         public Timer Timer;
         public bool IsPause;
-        public MedKit MedKit;
-        public Box Box;
-        public List<Zombie> Zombies;
+        private int counter;
+        public bool IsOver { get; private set; }
+        public bool IsWon { get; private set; }
+        public bool IsStarted { get; private set; }
         public static Random rnd = new Random();
         public Point MousePosition;
 
@@ -26,22 +32,31 @@ namespace CrazyKiller
         {
             Player = new Player();
             InitializeTimer();
-            Zombies = new List<Zombie>();
-            for (var i = 0; i < 20; i++)
-                Zombies.Add(new Zombie());
+            Zombies = new List<Golem>();
             MedKit = new MedKit();
             Box = new Box();
+            levels = new Level[9].Select((x, i) => new Level(i + 1)).ToArray();
+            currentLevel = levels?.FirstOrDefault();
+            InitializeZombies();
+        }
+
+        private void InitializeZombies()
+        {
+            for (var i = 0; i < currentLevel?.CountZombies; i++)
+            // for (var i = 0; i < 1; i++)
+                Zombies.Add(new Golem());
         }
 
         public void Start()
         {
             Timer.Start();
+            IsStarted = true;
         }
 
         private void InitializeTimer()
         {
             Timer = new Timer();
-            Timer.Interval = 16;
+            Timer.Interval = 45;
             Timer.Tick += TimerOnTick;
         }
 
@@ -49,19 +64,47 @@ namespace CrazyKiller
         {
             if (IsPause) return;
 
-            if (Zombies.Count == 0 || Player.Hp == 0)
-                Application.Exit();
-
             Player.Move();
+            Player.IsAttacked = false;
             foreach (var zombie in Zombies)
             {
+                zombie.IsAttack = false;
+                // zombie.IsPenetration = false;
                 zombie.Move(Player);
-                if (IsInteract(zombie, Player)) Player.AddHeath(-zombie.Damage);
+                if (IsInteract(zombie, Player))
+                {
+                    zombie.IsAttack = true;
+                    Player.AddHeath(-zombie.Damage);
+                    Player.IsAttacked = true;
+                }
             }
 
             Player.Gun.Shoot(Zombies);
             MedKit.Interact(Player);
             Box.Interact(Player);
+
+            ChangeLevel();
+
+            if (Player.Hp == 0) IsOver = true;
+        }
+
+        private void ChangeLevel()
+        {
+            if (Zombies.Count == 0)
+            {
+                if (levels is null || levels.Length == 0 || currentLevel.Number == levels.Length)
+                {
+                    IsOver = true;
+                    IsWon = true;
+                    return;
+                }
+
+                counter++;
+                if (counter < 100) return;
+                currentLevel = levels[currentLevel.Number];
+                InitializeZombies();
+                counter = 0;
+            }
         }
 
         public static bool IsInteract(object one, object two)
@@ -79,14 +122,9 @@ namespace CrazyKiller
             if (destination.X - size.Width / 2 > WindowSize.Width ||
                 destination.X - size.Width / 2 < 0 ||
                 destination.Y + size.Height / 2 > WindowSize.Height ||
-                destination.Y - size.Height / 2 < 0) 
+                destination.Y - size.Height / 2 < 0)
                 return false;
             return true;
-        }
-
-        public bool IsOccupied() // чтобы зомби не могли ходить в себя
-        {
-            return false;
         }
     }
 }
